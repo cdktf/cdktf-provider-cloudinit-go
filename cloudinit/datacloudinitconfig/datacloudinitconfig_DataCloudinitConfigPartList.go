04357ecd6a9018177d998448d1725a0e70e06cf7
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-01-17 14:56:04 +0000

    chore(release): v5.0.0

--- cloudinit/datacloudinitconfig/datacloudinitconfig_DataCloudinitConfigPartList.go
+++ cloudinit/datacloudinitconfig/datacloudinitconfig_DataCloudinitConfigPartList.go
@@ -2,9 +2,9 @@ package datacloudinitconfig
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-cloudinit-go/cloudinit/v4/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-cloudinit-go/cloudinit/v5/jsii"
 
-	"github.com/cdktf/cdktf-provider-cloudinit-go/cloudinit/v4/datacloudinitconfig/internal"
+	"github.com/cdktf/cdktf-provider-cloudinit-go/cloudinit/v5/datacloudinitconfig/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
